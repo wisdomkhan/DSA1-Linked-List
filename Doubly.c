1fd6d350ccbd9afd4784a3de49c14e124a76d7c5
Mohammad Asim Khan <ak4514768@gmail.com>
2021-06-28 13:27:32 +0530

    added delete functions in doubly

--- Doubly.c
+++ Doubly.c
@@ -6,24 +6,66 @@ struct node
     struct node *prev;
     struct node *next;
 };
-struct node *head = NULL, *last = NULL;
+struct node *head = NULL, *last = NULL; //last is same tail
 
 void createList(int n);
 void displayList();
 void insertAtBeginning(int data);
 void insertAtEnd(int data);
-void insertAtN(int data, int position);
+void insertAtN(int data, int pos);
+void delEnd();
+void delFront();
+void delAtn(int pos);
+
+void delEnd(){
+    if(head == NULL)
+        printf("empty list\n");
+    else{
+        struct node *temp;
+        temp = last -> prev;
+        temp -> next = NULL;
+        last -> prev = NULL;
+        last = temp;
+        printf("Node at end deleted.\n");
+        displayList();
+    }
+}
+
+void delFront(){
+    head -> next -> prev = NULL;
+    head = head ->next;
+    printf("Front node deleted\n");
+    displayList();
+}
+
+void delAtn(int pos){
+    int i = 1;
+    struct node *loop = head;
+    while(loop != NULL && i < pos){
+        loop = loop->next;
+        i++;
+    }
+    loop -> prev -> next = loop -> next;
+    loop -> prev = NULL;
+    loop -> next = NULL;
+    printf("Node %d deleted\n", pos);
+    displayList();
+}
 
 int main()
 {
-    int n, data, choice = 1;
+    int n, pos, data, choice = 1;
     while (choice != 0)
     {
+        printf("--------------------------------------------\n");
         printf("1. Create List\n");
         printf("2. Insert node - at beginning\n");
         printf("3. Insert node - at end\n");
         printf("4. Insert node - at N\n");
         printf("5. Display list\n");
+        printf("6. Delete at end\n");
+        printf("7. Delete at front\n");
+        printf("8. Delete at position\n");
         printf("0. Exit\n");
         printf("--------------------------------------------\n");
         printf("Enter your choice : ");
@@ -48,18 +90,36 @@ int main()
             break;
         case 4:
             printf("Enter the position where you want to insert new node: ");
-            scanf("%d", &n);
-            printf("Enter data of %d node : ", n);
+            scanf("%d", &pos);
+            printf("Enter data of %d node : ", pos);
             scanf("%d", &data);
-            insertAtN(data, n);
+            insertAtN(data, pos);
             break;
         case 5:
             displayList();
             break;
+        case 6:
+            delEnd();
+            break;
+        case 7:
+            delFront();
+            break;
+        case 8:
+            printf("Enter position of node to be deleted\n");
+            scanf("%d", &pos);
+            if(pos > 1 && pos < n)
+                delAtn(pos);
+            else if(pos==1)
+                delFront();
+            else if(pos==n)
+                delEnd();
+            else
+                printf("Invalid position");
+            break;
         case 0:
             break;
         default:
-            printf("Error! Invalid choice. Please choose between 0-5");
+            printf("Error! Invalid choice. Please choose between 0-8");
         }
         printf("\n");
     }
@@ -86,7 +146,7 @@ void createList(int n)
             }
             else
             {
-                printf("You are appending at the end of existing list\nEnter next node ");
+                printf("Enter next node ");
                 scanf("%d", &data);
                 newNode->data = data;
                 newNode->prev = last;
@@ -96,6 +156,7 @@ void createList(int n)
             }
         }
         printf("\nDOUBLY LINKED LIST CREATED SUCCESSFULLY\n");
+        displayList();
     }
 }
 
@@ -138,6 +199,7 @@ void insertAtBeginning(int data)
         head->prev = newNode;
         head = newNode;
         printf("NODE INSERTED SUCCESSFULLY AT THE BEGINNING OF THE LIST\n");
+        displayList();
     }
 }
 
@@ -158,10 +220,11 @@ void insertAtEnd(int data)
         last->next = newNode;
         last = newNode;
         printf("NODE INSERTED SUCCESSFULLY AT THE END OF LIST\n");
+        displayList();
     }
 }
 
-void insertAtN(int data, int position)
+void insertAtN(int data, int pos)
 {
     int i = 1;
     struct node *newNode, *temp = head;
@@ -172,12 +235,12 @@ void insertAtN(int data, int position)
     }
     else
     {
-        while (i < position - 1 && temp != NULL)
+        while (i < pos - 1 && temp != NULL)
         {
             temp = temp->next;
             i++;
         }
-        if (position == 1)
+        if (pos == 1)
             insertAtBeginning(data);
         else if (temp == last)
             insertAtEnd(data);
@@ -190,7 +253,8 @@ void insertAtN(int data, int position)
             if (temp->next != NULL) // checking if it is the end of list
                 temp->next->prev = newNode;
             temp->next = newNode;
-            printf("NODE INSERTED SUCCESSFULLY AT %d POSITION\n", position);
+            printf("NODE INSERTED SUCCESSFULLY AT %d POSITION\n", pos);
+            displayList();
         }
         else
         {
